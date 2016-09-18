c8398ae906de8c7bda2e6908b753f2941676edc5
Ashishgupta27 <gupta.ashish@siesgst.ac.in>
2016-09-18 21:05:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/activity_one', function (req,res){
-res.send('Activity one is served ')    ;
+res.sendFile(path.join(__dirname, 'activity_one', 'index.html'));
 });
 app.get('/activity_two', function (req,res){
 res.send('Activity two is served ')    ;

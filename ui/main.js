d60adb3d7dc22a67c07ac3868b32cd5af8f0f216
Ashishgupta27 <gupta.ashish@siesgst.ac.in>
2016-09-26 20:09:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onclick =function() {
        {
         if(request==200)
         {
-         counter=counter+1;
+          var counter=request.responseText;
 var span=document.getElementById('count');
 span.innerHTML=counter.toString();   
         }

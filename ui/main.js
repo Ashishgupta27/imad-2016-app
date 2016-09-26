6901a3a4a5810be447b6d7b99ba45ecfb3fdec85
Ashishgupta27 <gupta.ashish@siesgst.ac.in>
2016-09-26 20:56:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,15 @@ request.open('GET','http://ashishgupta27.imad.hasura-app.io/counter',true);
 request.send(null);
 
 };
+var name_value=document.getElementById('name');
+var name0=name_value.value;
+submit=document.getElementById('submit_btn')
+submit.onclick=function() {
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<=name.length;i++)
+    list += '<li>'+names[i]+'</li>';
+    var ul=document.getElementById('ulname');
+    ul.innerHTML=list;
+};
+

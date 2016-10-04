89d111e83d4ec881dfb5ad6b6a006c3b3c86449b
Ashishgupta27 <gupta.ashish@siesgst.ac.in>
2016-10-04 11:20:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,8 +14,13 @@ res.sendFile(path.join(__dirname, 'ui', 'Activity_one.html'));
 app.get('/activity_two', function (req,res){
 res.sendFile(path.join(__dirname, 'ui', 'Activity_two.html'));
 });
+
+
 app.get('/activity_three', function (req,res){
 res.sendFile(path.join(__dirname, 'ui', 'Activity_three.html'));
+});
+app.get('/home', function (req,res){
+res.sendFile(path.join(__dirname, 'ui', 'home.html'));
 });
 var counter=0;
 app.get('/counter', function (req,res){
